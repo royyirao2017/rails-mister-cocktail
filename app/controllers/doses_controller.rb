0b25e59111e1408dd857408baa3b7dac774994bc
roy rao <royyirao@qq.com>
2017-08-10 18:27:41 +0800

    cocktail file

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,29 @@
+class DosesController < ApplicationController
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to dose_path(@dose)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose.destroy
+    redirect_to dose_path
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id, :cocktail_id)
+  end
+
+end
